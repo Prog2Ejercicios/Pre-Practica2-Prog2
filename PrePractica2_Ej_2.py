688ec3cb2b9a508195c9e96005c6a1f3f0f82bca
Azul Cura <azulcura1@gmail.com>
2022-08-17 22:52:49 -0300

    Pre-Practica2

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,21 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
+lista=[]
+lista_impar=[]
+while True:
+    print("Ingrese un numero distinto de 0: ")
+    num=int(input())
+    if num!=0:
+        lista.append(num)
+    else:
+        break
+for num in lista:
+    if num %2!=0:
+        lista_impar.append(num)
+
+
+print("La lista es: " + str(lista))
+print("La lista impar es " + str(lista_impar))
 
 #FIN
