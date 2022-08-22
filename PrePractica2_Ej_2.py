689edae35266111d8962fb1622af085d98418807
Virginia-Vaccaro <vir.vaccaro@gmail.com>
2022-08-21 21:23:24 -0300

    Grupo 1 - Comision 4

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,20 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
-
+lista=[]
+listaImpares=[]
+print("Ingrese varios números enteros, para finalizar presione 0.")
+while True:
+    print("Ingresar número:")
+    numero=int(input())
+    if numero!=0:
+        lista.append(numero)
+        if numero%2!=0:
+           listaImpares.append(numero)
+    else:
+        break
+print("La lista de números originales es:")
+print(lista)
+print("La lista de números impares es:")
+print(listaImpares)
 #FIN
