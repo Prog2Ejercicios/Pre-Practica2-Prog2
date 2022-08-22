296c060e7bd8689ba24f2a725aed58429898bb69
Bruno Menna <brunocesarmenna@gmail.com>
2022-08-21 22:59:43 -0300

    realizados

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -1,8 +1,18 @@
-#Crear un programa que permita ingresar una lista de numeros al usuario y muestre por pantalla el maximo entre ambos numeros.
+#Crear un programa que permita ingresar dos numeros al usuario y muestre por pantalla el maximo entre ambos numeros.
 
 #Nota : Hacerlo con la función max(a,b) y luego con una comparación
 
 #INICIO
+print("Ingrese dos números: ")
+num1=int(input())
+num2=int(input())
+mayor=max(num1, num2)
+print(f"El mayor es: {mayor}")
 
-
+if num2>num1:
+    print(f"El mayor número es: {num2}")
+elif num2<num1:
+    print(f"El mayor número es: {num1}")
+else:
+    print("Ambos números son iguales")
 #FIN
--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,18 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
+print("Ingrese tamaño de la lista:")
+tam=int(input())
+print("Ingrese los valores en números enteros:")
+lista=[]
+lista_impar=[]
 
+while len(lista)!=tam:
+    num=int(input())
+    lista.append(num)
+    if num%2!=0:
+        lista_impar.append(num)
+
+print("Lista original:" ,lista)
+print("Lista de impares:", lista_impar)
 #FIN
