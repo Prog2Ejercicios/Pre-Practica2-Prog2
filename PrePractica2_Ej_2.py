3693e83e20cd490c474e36c924b107bce3e1f3b8
GiulianoPairone <yuliars.pairone@gmail.com>
2022-08-22 01:29:34 -0300

    Pre practica

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -4,4 +4,18 @@
 
 #INICIO
 
+lista=[]
+listaImpar=[]
+while True:
+    print("Ingrese un numero distinto de 0")
+    numero=int(input())
+    if numero != 0:
+        lista.append(numero)
+        if numero%2 != 0:
+            listaImpar.append(numero)
+    else:
+        break
+print("Lista Original"+ str(lista))
+print("Lista Impar"+ str(listaImpar))
+
 #FIN
