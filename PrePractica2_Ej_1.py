4cd6515abd70eea9c0e3b6be19545e51930298a2
Ayelen Romero <romeroayelenlaura@gmail.com>
2022-08-22 21:46:32 -0300

    Com4 - Grupo 7

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -3,6 +3,13 @@
 #Nota : Hacerlo con la función max(a,b) y luego con una comparación
 
 #INICIO
+a=int(input("Escribe un numero: "))
+b=int(input("Escribe otro numero: "))
+print("El mayor es: " + str(max(a,b)))
 
+if a>b: 
+    print("El mayor es: " +str (a))
+else:
+    print("El mayor es: " +str (b))
 
 #FIN
